867075c9513eba10bcda448dd1b78132fafc6189
Luis <Almeida>
2021-06-14 12:02:35 +0100

    404 page

--- aulas/src/app/app-routing.module.ts
+++ aulas/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { ReadMeGeneratorComponent } from './read-me-generator/read-me-generator.component';
 
 const routes: Routes = [
-  {path:"", component:HomeComponent},
+  {path:"", component: HomeComponent},
   {path:"home", component: HomeComponent},
   {path:"calculator", component: CalculadoraComponent},
   {path:"readMeGen", component: ReadMeGeneratorComponent},
